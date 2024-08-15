1b8df5ee75d6adf4fd10538e68e5244460682065
furqanzamir112 <furqan.zamir@systemsltd.com>
2024-08-15 19:01:45 +0500

    Javascript more function added for js file

--- index.js
+++ index.js
@@ -482,8 +482,250 @@
 
 //reduce function new array combine, new array create and addition, percentage, average etc
 //100 is a initial value we can pass one value eg for subject value in array and one value is string so we can pass as a initial value.
-let arr = [5,6,2];
-let newarr = arr.reduce((accum,curElm)=>{
-    return accum *=curElm;
-},100)
-console.log(newarr);
+// let arr = [5,6,2];
+// let newarr = arr.reduce((accum,curElm)=>{
+//     return accum *=curElm;
+// },100)
+// console.log(newarr);
+
+// Array inside array we have to check another method
+// const arr =[
+//     ['zone_1','zone_2'],
+//     ['zone_3','zone_4'],
+//     ['zone_5','zone_6'],
+//     ['zone_7','zone_8',['zone_5','zone_6']]
+// ];
+
+// let mynewarr = arr.reduce((accum,curnval)=>{
+//     return accum.concat(curnval);
+// })
+// console.log(mynewarr);
+
+//String, Used single quote and double quote, storing and manipulating text
+
+// let myname = "Furqan Zamir";
+// let myjobtitle = "Consultant Frontend Developer";
+
+// let ytname = new String("Furqan Zamir");
+
+// console.log(myname);
+// console.log(myjobtitle);
+// console.log(ytname); // string convert into array or split in character wise
+
+//String Length 
+// let myname = "Furqan Zamir";
+// console.log(myname.length);
+
+//Escape Character
+// let myname = 'furqan zamir \'Consultant developer\'';
+// console.log(myname);
+
+// finding a string in a string
+
+// let myname = 'furqan zamir Consultant developer';
+// console.log(myname.indexOf("t",13));
+
+//Last Index of find in the last element of the string
+// let myname = 'furqan zamir Consultant developer';
+// console.log(myname.lastIndexOf("t"));
+
+//Searching for a string in a String // output where match the string the show the index of the starting string.
+// let myname = 'furqan zamir Consultant developer';
+// console.log(myname.search("zamir"))
+
+//there are 3 method for extracting a part of a string:
+// slice(start, end) // slide extract a part of a string and return the extracted part in a new string
+// substring(start, end)
+// substr(start, end)
+
+//In slice last index will not show  
+// let myname = 'Apple, Banana, Kiwi, Mango';
+// //let res = myname.slice(0,5);
+// //let res = myname.slice(5,-2);
+// console.log(res);
+
+//Create code and limit 280 character 
+
+// let mystring="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+// let res = mystring.slice(0,280);
+// console.log(res.length);
+// console.log(res);
+
+//substring is similar to slice but negation can not add
+// let myname = 'Apple, Banana, Kiwi, Mango';
+// //let res = myname.substring(0,4);
+// let res = myname.substring(7,-2); // start 7 character
+// console.log(res);
+
+
+// the substr() method
+// let myname = 'Apple, Banana, Kiwi, Mango';
+// let res = myname.substr(0,4); 
+// //let res = myname.substr(-5); //output Mango
+// console.log(res); //output Appl
+
+//Replacing string content
+// let mynewarr="Furqan Zamir";
+// let myres = mynewarr.replace("Furqan","zamir")
+// console.log(myres); //Output zamir Zamir 
+// console.log(mynewarr); //output Origina data never change
+
+//Extracting Character Method
+// let str = "Hello World"
+// console.log(str.charAt(0)); //output H
+
+//charcodeAt return the unicode of the string 0 to 65535
+// let str = "Hello World"
+// console.log(str.charCodeAt(0)); //output H unicode 72
+
+//Last Character find from the string
+// let str = "Hello World"
+// let strlength = str.length -1;
+// console.log(str.charCodeAt(strlength)); //output d unicode 100
+
+//Character string as a Array 
+// var str = "HELLO WORLD";
+// console.log(str[2]); //Output L 
+
+
+//TO upercase and ToLowercase 
+// var str = "Furqan Zamir";
+// //console.log(str.toLowerCase()); // Output furqan zamir
+// console.log(str.toUpperCase()); // Output FURQAN ZAMIR
+
+//Concat Method in String
+// let fName = "Furqan Zamir"
+// let lName = "Zamir Ahmed"
+// console.log(`${fName} and ${lName}`)
+// console.log(fName + " " +lName );
+// console.log(fName.concat(lName));
+// console.log(fName.concat(" ",lName));
+
+//String trim method remove starting and end remove the white space
+// let str = "          HELLO WORLD"
+// console.log(str.trim());
+
+//String convert into array we used split
+// let str = "abcdef"
+// console.log(str.split(","));
+// console.log(str.split(" "));
+// console.log(str.split("|"));
+
+//Split with space bar or without space they convert into array
+// let text = "How are you doing |today?.";
+// const myArray = text.split("");
+// const myArray2 = text.split(",");
+// console.log(myArray);
+// console.log(myArray2);
+
+//Date and Time in Javascript Four formate for Date new Date Constructor
+// new Date();
+// new Date(year, month, day, hours, minutes, seconds, miliseconds)
+// new Date(miliseconds)
+// new Date(date string)
+
+//let currentDate = new Date();
+// console.log(currentDate); //Thu Aug 15 2024 13:47:50 GMT+0500 (Pakistan Standard Time)
+// console.log(currentDate.toLocaleDateString()); //8/15/2024
+// console.log(currentDate.toString()); //Thu Aug 15 2024 13:47:50 GMT+0500 (Pakistan Standard Time)
+// console.log(currentDate.toLocaleString(2024, 11)); // 8/15/2024, 1:54:05 PM
+
+
+//Date method 
+// console.log(currentDate.toLocaleString()) //8/15/2024, 2:09:43 PM
+// console.log(currentDate.getFullYear()) //2024
+// console.log(currentDate.getMonth()) //7
+// console.log(currentDate.getDate())// 15
+// console.log(currentDate.getDay()) // 4
+
+//set Date method means its show in milisecond
+// console.log(currentDate.setFullYear(2024))
+// console.log(currentDate.setMonth(10))
+// console.log(currentDate.setDate(15))
+// console.log(currentDate.toLocaleString(10))
+
+//Time Methods
+
+// const curTime = new Date()
+// console.log(currentDate.getTime(2024))
+// console.log(currentDate.getHours(10))
+// console.log(currentDate.getMinutes(15))
+// console.log(currentDate.getMilliseconds(10))
+
+//const curTime = new Date()
+//console.log(currentDate.setTime())
+// console.log(currentDate.setHours(10))
+// console.log(currentDate.setMinutes(15))
+// console.log(currentDate.setMilliseconds(10))
+
+//Only Time
+//console.log(curTime.toLocaleTimeString());
+
+//Only Date
+//console.log(curTime.toLocaleDateString());
+
+//Only Houes
+//console.log(curTime.toLocaleString());
+
+//To Math Object in Javascript
+
+// console.log(Math.PI); //Output 3.14159
+// console.log(Math.round(10.52598)); //output 11 10.5 means its 11 in round
+// console.log(Math.pow(2,4))// output 16 e.g 2*2*2*2
+
+//Math.sqrt
+// console.log(Math.sqrt(25));
+// console.log(Math.abs(-10)); //abs means absolute positive number
+// console.log(Math.abs(-55)); //abs means absolute positive number
+// console.log(Math.abs(-0.59898)); //abs means absolute positive number
+// console.log(Math.abs(10-80)); //abs means absolute positive number
+
+//Math.ceil return the value of x rounded up to its nearest integer after 1.2 means next number will show 2
+// console.log(Math.ceil(2.1)); //output 3
+// console.log(Math.round(2.1)); //output 2
+
+//Math.floor() return befor decimal show that number
+//console.log(Math.floor(2.8)); //output 2
+
+//Math.min minimum number
+//console.log(Math.min(10,55,-10,-59,-47)); //output -59
+
+//Math.max maximum number
+//console.log(Math.max(10,55,-10,-59,-47)); //output 55
+
+//Math.roundom()
+//Math.random() return a random number between 0 (inclusive), and 1 (included)
+
+// console.log(Math.random()*10); // 0 to 9 output 2.4073445470084898
+// console.log(Math.floor(Math.random()*10)); // output always before . digit 2 1 7 9
+
+//Math.trunc()
+//The trunc() method returns the integer part of a number
+
+// console.log(Math.trunc(4.6));
+// console.log(Math.trunc(-99.1));
+
+//DOM in Javascript
+//window is a container and global object, entire browser window
+//All the member like object,methods or properties
+
+//Document means body part of the object or child of window object
+
+//window child related means inside the Html
+//BOM browser object model
+//BOM related to the window navigator, screen, location,frames,history XML.HttpRequest
+
+// let divselect = document.querySelector('body');
+// divselect.style.backgroundColor ="#494949"
+
+// let firstchildElm = document.body.children;
+// console.log(firstchildElm) // Output div.container
+
+// let parentelem = document.body.parentElement
+// console.log(parentelem) // html
+
+// let parentelem = document.body.previousSibling
+// console.log(parentelem) // html
+
+
+
